c927b2092c707d59acabbf70764eea79619b06ba
jalbrekt85 <jcalbrecht85@gmail.com>
2025-01-24 08:41:29 -0600

    mock mimic data

---
+++ fee_allocator/fees_collected/get_report.py
@@ -0,0 +1,60 @@
+import json
+from datetime import datetime, timedelta
+
+import requests
+
+
+BASE_URL = "https://api.mimic.fi/public/summary/"
+ENV_ID = "0xd28bd4e036df02abce84bc34ede2a63abcefa0567ff2d923f01c24633262c7f8"
+CHAIN_ID = "1"
+
+
+def get_report(start_date, end_date):
+    return {
+        "zkevm": 4338269978,
+        "arbitrum": 36687801536,
+        "avalanche": 4338269978,
+        "polygon": 19554769296,
+        "base": 45724823982,
+        "gnosis": 55378154706,
+        "mainnet": 374340881277
+    }
+    # docs: https://mimic-fi.notion.site/Balancer-API-explanation-1289958dbf4d80beb76ad13462898fee
+    response = requests.get(
+        f"{BASE_URL}{ENV_ID}",
+        params={
+            "envId": ENV_ID,
+            "chainId": CHAIN_ID,
+            "startDate": start_date,
+            "endDate": end_date,
+        },
+    )
+    response.raise_for_status()
+    # breakpoint()
+    report = response.json()["depositors"]
+    total = 0
+    for chain, amount in report.items():
+        report[chain] = int(amount)
+        total += int(amount)
+    if total > 0:
+        return report
+    else:
+        raise ValueError("Sum of collected fees is not > 0")
+
+
+if __name__ == "__main__":
+    # run this every other thursday after the end of an epoch
+    today = datetime.now()
+
+    if bool(int(today.strftime("%V")) % 2):
+        # week number is uneven; there should be a new report
+
+        yesterday = today - timedelta(days=1)
+        epoch_start = today - timedelta(days=14)
+
+        report = get_report(yesterday.strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d"))
+        with open(
+            f"fee_allocator/fees_collected/fees_{epoch_start.strftime('%Y-%m-%d')}_{today.strftime('%Y-%m-%d')}.json",
+            "w",
+        ) as f:
+            json.dump(report, f, indent=2)
